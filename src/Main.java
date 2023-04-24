37613a89d4dbcb4bbe5d6609119bf5601019846c
XSRoseBX <moushaya@kkg-do.eu>
2023-04-24 20:47:43 +0200

    Tasks

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+    public static void printSomeNumbers() {
+        for (int i = 6; i < 24; i++) {
+            System.out.println (i);
+        }
+    }
+    public static void printNumbers(int start, int end) {
+        for (int i = start; i < end+1; i++) {
+            System.out.println (i);
+        }
+    }
+    public static int sumUp(int start, int end) {
+        int result = 0;
+        for (int i = 6; i < 24; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+}
